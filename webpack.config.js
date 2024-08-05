06c7115dd5142482f5a6073b937b0bcbca6531fe
1-Ishan <154886629+1-Ishan@users.noreply.github.com>
2024-08-05 18:22:21 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Entry point of your application
+  output: {
+    filename: 'bundle.js', // Output file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply this rule to .js files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], // Use the preset-env Babel preset
+          },
+        },
+      },
+    ],
+  },
+};
